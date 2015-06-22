922acf7451cde864456bfba17c45fad7944930ea
Julien Paillé <julienp@openfun.fr>
2015-06-22 14:11:53 +0000

    update conf.

--- init.el
+++ init.el
@@ -58,9 +58,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(completion-ignored-extensions (quote (".pyc" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc")))
+ '(completion-ignored-extensions
+   (quote
+    (".pyc" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/" "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl" ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux" ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc")))
  '(cua-mode t nil (cua-base))
- '(dired-garbage-files-regexp "\\(?:\\.\\(?:aux\\|bak\\|dvi\\|pyc\\|log\\|orig\\|rej\\|toc\\)\\)\\'")
+ '(dired-garbage-files-regexp
+   "\\(?:\\.\\(?:aux\\|bak\\|dvi\\|pyc\\|log\\|orig\\|rej\\|toc\\)\\)\\'")
  '(dired-omit-files "^\\.?#\\$")
  '(display-time-mode t)
  '(flycheck-idle-change-delay 0)
@@ -101,6 +104,7 @@
  '(magit-log-head-label-local ((t (:foreground "LightSkyBlue4" :box 1))))
  '(magit-log-head-label-remote ((t (:foreground "OliveDrab4" :box 1))))
  '(magit-tag ((t (:foreground "goldenrod4"))))
+ '(match ((t (:foreground "magenta"))))
  '(minibuffer-prompt ((t (:foreground "red"))))
  '(web-mode-html-tag-custom-face ((t (:inherit web-mode-warning-face))))
  '(web-mode-html-tag-face ((t (:foreground "red")))))
